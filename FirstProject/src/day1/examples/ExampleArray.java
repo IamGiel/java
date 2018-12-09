e826207f4e4baf78ce9655f22cb1aade2b3630e1
IamGiel <november14giel@gmail.com>
2018-12-09 13:03:31 -0500

    looping through values in an array

--- FirstProject/src/day1/examples/ExampleArray.java
+++ FirstProject/src/day1/examples/ExampleArray.java
@@ -9,9 +9,21 @@ public class ExampleArray {
 //		int c = 30;
 		int[] a = {10, 20, 30, 40, 50};
 		System.out.println(a[0] + a[2]);
-		System.out.println("-----------");
+		System.out.println("-----looping through values in an array------");
 		for(int temp: a) {
 			System.out.println(temp);
+		}
+		
+		int[] x = new int[10];
+		x[3] = 25;
+		System.out.println("----looping through integers and insert a new value on a cetain index of the array-------");
+		for(int temp2: x) {
+			System.out.println(temp2);
+		}
+		System.out.println("-----looping through strings------");
+		String[] st = {"one", "two", "three"};
+		for(String temp3: st) {
+			System.out.println(temp3);
 		}
 	}
 
